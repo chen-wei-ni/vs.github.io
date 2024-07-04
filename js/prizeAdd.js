556568eaf78fa57e95dc384f26968ee3be5e0273
chen-wei-ni <bcg2135@gmail.com>
2024-07-04 13:28:52 +0800

    title pictures modified

--- js/prizeAdd.js
+++ js/prizeAdd.js
@@ -71,21 +71,21 @@ anime({
     },
 });
 
-let pz0 = 1000000; //原本金額
-let pz1 = 1000600; //新金額
+let pz0 = 1000000.22; //原本金額
+let pz1 = 1000600.66; //新金額
 anime({
     targets: ".crazy-time .award",
     innerHTML: [pz0, pz1],
     easing: "steps(35)",
     duration: 2000,
-    round: 1,
+    round: 20,
 });
-let aw0 = 1200000; //原本金額
-let aw1 = 1208600; //新金額
+let aw0 = 1200000.22; //原本金額
+let aw1 = 1208600.66; //新金額
 anime({
     targets: ".jackpot .award",
     innerHTML: [aw0, aw1],
     easing: "steps(35)",
     duration: 2000,
-    round: 1,
+    round: 20,
 });
