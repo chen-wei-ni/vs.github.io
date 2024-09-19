46b12922c40dd77bc96dfa0840b1afc7a7a53a01
chen-wei-ni <bcg2135@gmail.com>
2024-09-19 14:43:17 +0800

    new button

--- js/vs/eventPopup.js
+++ js/vs/eventPopup.js
@@ -1,6 +1,8 @@
-const openBtn = document.querySelector(".btn");
-const closeBtn = document.querySelector(".close-btn");
-const popup = document.querySelector(".event-desc");
+const openBtn = document.getElementById("event-btn");
+const openBtn2 = document.getElementById("history-btn")
+const closeBtn = document.querySelectorAll(".close-btn");
+const popup = document.getElementById("event");
+const popup2 = document.getElementById("history");
 
 openBtn.addEventListener("click", (e) => {
     const block = popup.querySelector(".block");
@@ -8,13 +10,28 @@ openBtn.addEventListener("click", (e) => {
     popup.animate(opacityAnimate, scaleTiming);
     block.animate(scaleAnimate, scaleTiming)
     document.body.style.overflow = "hidden"
+});
+openBtn2.addEventListener("click", (e) => {
+    const block = popup2.querySelector(".block");
+    popup2.style.display = "grid";
+    popup2.animate(opacityAnimate, scaleTiming);
+    block.animate(scaleAnimate, scaleTiming)
+    document.body.style.overflow = "hidden"
 })
-closeBtn.addEventListener("click", () => {
-    popup.animate(opacityClose, scaleTiming)
-    setTimeout(() => {
-        popup.style.display = "none";
-    }, 180);
-    document.body.style.overflow = "";
+
+
+closeBtn.forEach((e) => {
+    e.addEventListener("click", function () {
+        popup.animate(opacityClose, scaleTiming)
+        setTimeout(() => {
+            popup.style.display = "none";
+        }, 180);
+        popup2.animate(opacityClose, scaleTiming)
+        setTimeout(() => {
+            popup2.style.display = "none";
+        }, 180);
+        document.body.style.overflow = "";
+    })
 })
 
 const opacityAnimate = [
